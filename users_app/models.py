f183a8318caf92e90e35bd2102d44007fbacb8d0
TagRomEdu <qwertystyles@gmail.com>
2023-11-10 09:54:06 +0500

    created users_app and migrated it

---
+++ users_app/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(_("email"), unique=True)
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150)
+    phone = models.IntegerField(_("phone"), **NULLABLE)
+    city = models.CharField(_("city"), **NULLABLE, max_length=100)
+    avatar = models.ImageField(_("avatar"), **NULLABLE, upload_to='media/')
+
+    USERNAME_FIELD =  'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
