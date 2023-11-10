5fc72cc622628df23e2a32a5dd332d6f53f9c843
TagRomEdu <qwertystyles@gmail.com>
2023-11-10 10:52:59 +0500

    added urls for Lesson's view

--- education_app/urls.py
+++ education_app/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
+
 from education_app.apps import EducationAppConfig
 from rest_framework.routers import DefaultRouter
 
-from education_app.views import CourseViewSet
+from education_app.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = EducationAppConfig.name
 
@@ -9,5 +12,10 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
+    path('create_lesson', LessonCreateAPIView.as_view(), name='create_lesson'),
+    path('list_lesson', LessonListAPIView.as_view(), name='list_lesson'),
+    path('list_lesson/int:<pk>', LessonRetrieveAPIView.as_view(), name='single_lesson'),
+    path('update_lesson/int:<pk>', LessonUpdateAPIView.as_view(), name='update_lesson'),
+    path('delete_lesson', LessonDestroyAPIView.as_view(), name='delete_lesson'),
 
 ] + router.urls
