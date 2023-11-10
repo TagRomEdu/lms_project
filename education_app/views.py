aa21297583593133fcb4e88f2baf5dee1419f418
TagRomEdu <qwertystyles@gmail.com>
2023-11-10 10:28:53 +0500

    created view for Course with ViewSet

--- education_app/views.py
+++ education_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from education_app.models import Course
+from education_app.serializers import CourseSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
