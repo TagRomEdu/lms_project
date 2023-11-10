b02efea3a479729656a09084e72c5b6fcb2f8b05
TagRomEdu <qwertystyles@gmail.com>
2023-11-10 11:01:09 +0500

    fixed urls

--- education_app/urls.py
+++ education_app/urls.py
@@ -12,10 +12,10 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-    path('create_lesson', LessonCreateAPIView.as_view(), name='create_lesson'),
-    path('list_lesson', LessonListAPIView.as_view(), name='list_lesson'),
-    path('list_lesson/int:<pk>', LessonRetrieveAPIView.as_view(), name='single_lesson'),
-    path('update_lesson/int:<pk>', LessonUpdateAPIView.as_view(), name='update_lesson'),
-    path('delete_lesson', LessonDestroyAPIView.as_view(), name='delete_lesson'),
+    path('create_lesson/', LessonCreateAPIView.as_view(), name='create_lesson'),
+    path('list_lesson/', LessonListAPIView.as_view(), name='list_lesson'),
+    path('list_lesson/<int:pk>', LessonRetrieveAPIView.as_view(), name='single_lesson'),
+    path('update_lesson/<int:pk>', LessonUpdateAPIView.as_view(), name='update_lesson'),
+    path('delete_lesson/<int:pk>', LessonDestroyAPIView.as_view(), name='delete_lesson'),
 
 ] + router.urls
