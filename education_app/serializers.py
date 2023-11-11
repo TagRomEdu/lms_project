4c59500ccd757c7e9e6583ce68b99c9088fccc0e
TagRomEdu <qwertystyles@gmail.com>
2023-11-11 19:25:07 +0500

    added list of lesson to serializer of course

--- education_app/serializers.py
+++ education_app/serializers.py
@@ -12,7 +12,16 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.IntegerField(source='lesson_set.count', required=False)
+    lessons = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lessons(self, obj):
+        if obj.lesson_set.all():
+            lesson_list = [lesson.title for lesson in obj.lesson_set.all()]
+            return lesson_list
+        return 'There are no lessons in the course'
+
+
