b7aa9694b87f1a05ca3587dcdae515d9d5c6a319
TagRomEdu <qwertystyles@gmail.com>
2023-11-11 19:00:14 +0500

    created command for creating test payment

---
+++ payment_app/management/commands/add_pm.py
@@ -0,0 +1,16 @@
+from django.core.management import BaseCommand
+
+from education_app.models import Course
+from payment_app.models import Payment
+from users_app.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        payment = Payment.objects.create(
+            user=User.objects.get(pk=1),
+            paid_course=Course.objects.get(pk=2),
+            amount=100,
+            method='cash'
+        )
+        payment.save()
