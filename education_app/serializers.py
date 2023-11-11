69bc4e3a36edc6b2a3b61e22f8799203f23a43ee
TagRomEdu <qwertystyles@gmail.com>
2023-11-11 14:35:22 +0500

    added to CourseSerializer counting lessons

--- education_app/serializers.py
+++ education_app/serializers.py
@@ -3,15 +3,16 @@ from rest_framework import serializers
 from education_app.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
+        fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.IntegerField(source='lesson_set.count', required=False)
+
+    class Meta:
+        model = Course
         fields = '__all__'
