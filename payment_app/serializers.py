b015a816774334004dba7792bf92d9819e4c0be8
TagRomEdu <qwertystyles@gmail.com>
2023-11-12 11:23:03 +0500

    fixed: replace filter's settings from serializer to view

--- payment_app/serializers.py
+++ payment_app/serializers.py
@@ -1,6 +1,4 @@
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import serializers
-from rest_framework.filters import OrderingFilter
 
 from payment_app.models import Payment
 
@@ -9,6 +7,3 @@ class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = '__all__'
-        filter_backends = [OrderingFilter, DjangoFilterBackend]
-        filterset_fields = ('paid_course', 'paid_letter', 'method')
-        ordering_fields = ('date',)
--- payment_app/views.py
+++ payment_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.filters import OrderingFilter
 
 from payment_app.models import Payment
 from payment_app.serializers import PaymentSerializer
@@ -8,3 +10,6 @@ from payment_app.serializers import PaymentSerializer
 class PaymentListAPIView(generics.ListAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
+    filterset_fields = ('paid_course', 'paid_letter', 'method')
+    ordering_fields = ('date',)
