ff446150385ff3911a4e49c8ee90427442cacfd9
TagRomEdu <qwertystyles@gmail.com>
2023-11-14 13:51:07 +0500

    fixed command

--- users_app/management/commands/create_su.py
+++ users_app/management/commands/create_su.py
@@ -8,6 +8,9 @@ class Command(BaseCommand):
         user = User.objects.create(
             email='m@sster.ru',
             first_name='Roman',
-            last_name='Tagirov'
+            last_name='Tagirov',
+            is_superuser=True,
+            is_staff=True
         )
+        user.set_password('Aa12345!')
         user.save()
