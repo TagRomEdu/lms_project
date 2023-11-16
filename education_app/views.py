90fa232dc38ccd7e07636cbc030f16e0bbe200a3
TagRomEdu <qwertystyles@gmail.com>
2023-11-16 12:26:20 +0500

    added to course's viewset func to subscribe and unsubscribe

--- education_app/views.py
+++ education_app/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from education_app.models import Course, Lesson
+from education_app.models import Course, Lesson, Subscription
 from education_app.paginators import EducationPaginator
 from education_app.permissions import IsModerator, IsOwner
 from education_app.serializers import CourseSerializer, LessonSerializer
@@ -18,6 +20,18 @@ class CourseViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         return [permission() for permission in self.permission_classes_by_action.get(self.action, [IsAuthenticated])]
+
+    @action(detail=True, methods=['post'])
+    def subscribe(self, request, *args, **kwargs):
+        course = self.get_object()
+        Subscription.objects.get_or_create(user=request.user, course=course)
+        return Response({'detail': 'Subscription created'}, status=status.HTTP_201_CREATED)
+
+    @action(detail=True, methods=['delete'])
+    def unsubscribe(self, request, *args, **kwargs):
+        course = self.get_object()
+        Subscription.objects.filter(user=request.user, course=course).delete()
+        return Response({'detail': 'Subscription deleted'}, status=status.HTTP_204_NO_CONTENT)
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
