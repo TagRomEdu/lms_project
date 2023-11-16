5068f8bb7b7a82fef7c02e22d8443a48e6fa2f4f
TagRomEdu <qwertystyles@gmail.com>
2023-11-16 12:21:11 +0500

    fixed permission

--- education_app/permissions.py
+++ education_app/permissions.py
@@ -10,4 +10,4 @@ class IsModerator(BasePermission):
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        return obj.owner == request.user or request.user.is_superuser
