8424d75ffa3793bd3a1d4f1c04c1f15237d0dced
TagRomEdu <qwertystyles@gmail.com>
2023-11-16 11:34:54 +0500

    created model subscription

--- education_app/models.py
+++ education_app/models.py
@@ -33,3 +33,9 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = "Урок"
         verbose_name_plural = "Уроки"
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
