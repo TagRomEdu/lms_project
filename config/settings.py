db650669099a32aa058e60535968a946d433e254
TagRomEdu <qwertystyles@gmail.com>
2023-11-21 13:53:28 +0500

    added settings for swagger with authenticate by token

--- config/settings.py
+++ config/settings.py
@@ -148,3 +148,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated']
 }
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+        },
+    },
+}
