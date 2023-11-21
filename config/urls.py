dee0cd4596bf3ce178ffde506f0c0cd984944c18
TagRomEdu <qwertystyles@gmail.com>
2023-11-21 12:40:30 +0500

    added swagger

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('education/', include('education_app.urls', namespace='education')),
     path('payments/', include('payment_app.urls', namespace='payments')),
-    path('users/', include('users_app.urls', namespace='users'))
+    path('users/', include('users_app.urls', namespace='users')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
