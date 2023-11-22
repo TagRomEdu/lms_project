85b8a8bd5a8e5728fb6648bb2b22f55c5b33da57
TagRomEdu <qwertystyles@gmail.com>
2023-11-22 09:42:54 +0500

    created serializer for creating payments with stripe

--- payment_app/serializers.py
+++ payment_app/serializers.py
@@ -1,3 +1,6 @@
+import os
+
+import stripe
 from rest_framework import serializers
 
 from payment_app.models import Payment
@@ -7,3 +10,25 @@ class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = '__all__'
+
+
+class PaymentCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = '__all__'
+
+    def create(self, validated_data):
+        new_payment = Payment.objects.create(**validated_data)
+
+        description = f'Course: {new_payment.paid_course}' if new_payment.paid_course \
+            else f'Lesson: {new_payment.paid_lesson}'
+
+        stripe.api_key = os.getenv("STRIPE_API_KEY")
+        stripe.PaymentIntent.create(
+            amount=new_payment.amount,
+            currency='usd',
+            automatic_payment_methods={"enabled": True},
+            description=description
+        )
+        return new_payment
