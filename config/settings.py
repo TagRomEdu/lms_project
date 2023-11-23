7a3d21f535abd766ff751d5ff71e2356da970c3d
TagRomEdu <qwertystyles@gmail.com>
2023-11-23 11:12:51 +0500

    Added settings for redis as broker for celery

--- config/settings.py
+++ config/settings.py
@@ -158,3 +158,17 @@ SWAGGER_SETTINGS = {
         },
     },
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379'  # Например, Redis, который по умолчанию работает на порту 6379
+
+# URL-адрес брокера результатов, также Redis
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+# Часовой пояс для работы Celery
+CELERY_TIMEZONE = "Australia/Tasmania"
+
+# Флаг отслеживания выполнения задач
+CELERY_TASK_TRACK_STARTED = True
+
+# Максимальное время на выполнение задачи
+CELERY_TASK_TIME_LIMIT = 30 * 60
